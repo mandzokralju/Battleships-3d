483f02c7c132455b0a304a284c71bed8d46ff5ee
mandzokralju <rober.jan22@gmail.com>
2017-11-22 22:18:07 +0100

    Camera rotation fix

--- Assets/Scripts/CameraView.cs
+++ Assets/Scripts/CameraView.cs
@@ -6,25 +6,16 @@ public class CameraView : MonoBehaviour {
 
     public float speedH = 2.0f;
     public float speedV = 2.0f;
-    public Transform lookat;
 
-    private float yaw = 0.0f;
-    private float pitch = 0.0f;
-
-
-    private void start()
-    {
-        transform.position = GameObject.Find("CameraPoint").transform.position;
-    }
+    private float horizontal = 180.0f;
+    private float vertical = 0.0f;
 
     private void Update()
     {
-        transform.position = GameObject.Find("CameraPoint").transform.position;
+        horizontal += speedH * Input.GetAxis("Mouse X");
+        vertical -= speedV * Input.GetAxis("Mouse Y");
 
-        yaw += speedH * Input.GetAxis("Mouse X");
-        pitch -= speedV * Input.GetAxis("Mouse Y");
-
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        transform.eulerAngles = new Vector3(vertical, horizontal, 0.0f);
 
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
             GetComponent<Camera>().fieldOfView--;
