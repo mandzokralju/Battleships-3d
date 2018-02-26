da0552a2ef8cf7d670908e6aed3b6dac56acabdf
mandzokralju <rober.jan22@gmail.com>
2018-02-26 19:40:44 +0100

    Added test enemy

---
+++ Assets/AI_Capone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Capone : MonoBehaviour {
+
+	public ParticleSystem explosion;
+	public GameObject fractured;
+
+	void Start () {
+		Invoke ("Destruction", 3);
+	}
+	
+	void FixedUpdate () {
+		
+	}
+
+	void Destruction(){
+		explosion.transform.SetPositionAndRotation (new Vector3(
+			transform.position.x,
+			transform.position.y + 2,
+			transform.position.z),
+			transform.rotation);
+		
+		explosion.Play;
+		Instantiate (fractured, transform.position, transform.rotation);
+		Destroy ();
+	}
+}
