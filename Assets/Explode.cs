403b9bb2770654472562f501bb51d9a340b3049c
mandzokralju <rober.jan22@gmail.com>
2018-03-01 21:53:24 +0100

    Added ship destruction

---
+++ Assets/Explode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+    public ParticleSystem explosion;
+
+    void Start () {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, Constants.explosionRadius);
+
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(800, transform.position, Constants.explosionRadius, 2.0f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AI_Capone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Capone : MonoBehaviour {
+	
+	public GameObject fractured;
+    
+    private Vector3 shipPoz;
+    private Quaternion shipRot;
+
+	void Start () {
+		Invoke ("Destruction", 2);
+	}
+	
+	void FixedUpdate () {
+		
+	}
+
+	void Destruction(){
+        shipPoz = transform.position;
+        shipRot = transform.rotation;
+
+        Destroy(gameObject);
+        Instantiate(fractured, shipPoz, shipRot);	
+	}
+}
